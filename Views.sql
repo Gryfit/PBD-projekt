f8c506ebe5e827c11dc6bf7adf996095ac65e677
karolBak <bak.karolek@gmail.com>
2018-01-11 15:04:42 +0100

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -44,3 +44,38 @@ AS
        ON t.OrderID = o.OrderID
   GROUP BY t.ConferenceDayID, o.OrderID, OrderDate, Email, Phone, 
 GO
+
+CREATE VIEW VIEW_ConferencePopularity
+AS
+  SELECT c.ConferenceID, COUNT(c.ConferenceID) AS 'SeatsReserved', 
+         c.Seats - COUNT(c.ConferenceID) AS 'SeatsLeft', c.Seats
+  FROM Conferences AS c
+  JOIN Tickets AS t
+       ON t.ConferenceID = c.ConferenceID
+  WHERE t.OrderID != NULL
+  GROUP BY c.ConferenceID, Seats
+GO
+
+CREATE VIEW VIEW_ConferencePopularity
+AS
+  SELECT c.ConferenceID, (COUNT(c.ConferenceID) / c.Seats * 100) AS 'PercentSold',
+         COUNT(c.ConferenceID) AS 'SeatsSold', c.Seats
+  FROM Conferences AS c
+  JOIN Tickets AS t
+       ON t.ConferenceID = c.ConferenceID
+  WHERE t.OrderID != NULL
+  GROUP BY c.ConferenceID, Seats
+GO
+
+CREATE VIEW VIEW_WorkshopPopularity
+AS
+  SELECT wr.WorkshopID, (COUNT(wr.WorkshopID) * 100 / w.Seats) AS 'PercentSold',
+         COUNT(wr.WorkshopID) AS 'SeatsSold', w.Seats
+  FROM WorkshopReservations AS wr
+  JOIN Tickets AS t
+       ON t.TicketID = wr.WorkshopID
+  JOIN Workshops AS w
+       ON wr.WorkshopID = w.WorkshopID
+  WHERE t.OrderID != NULL
+  GROUP BY wr.WorkshopID, w.Seats
+GO
