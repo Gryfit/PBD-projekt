1ed0131c25e2af9c547ea1dba3890e4e41c3e779
Bartek <bartektonia@gmail.com>
2018-01-15 23:01:46 +0100

    Add files via upload

---
+++ Procedures2.sql
@@ -0,0 +1,24 @@
+CREATE PROC GenerateDays 
+@ConferenceID int
+AS
+	DECLARE
+	@StD datetime
+	DECLARE
+	@EnD datetime
+	DECLARE
+	@i int
+	SET @i =1
+	SET @StD = (SELECT TOP 1 StartDate
+	FROM Conferences
+	WHERE Conferences.ConferenceID = @ConferenceID)
+
+	SET @EnD = (SELECT TOP 1 EndDate
+	FROM Conferences
+	WHERE Conferences.ConferenceID = @ConferenceID)
+
+	WHILE @StD < @EnD
+	BEGIN
+		INSERT ConferenceDays Values (@ConferenceID,@i,@StD)
+		SET @i = (@i +1)
+		SET @StD = DATEADD(day,1,@StD)
+	END
