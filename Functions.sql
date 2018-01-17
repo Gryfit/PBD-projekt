4c539b9cf47596ef5030b0dd5e0cdb39759a6f9a
karolBak <bak.karolek@gmail.com>
2018-01-18 00:40:16 +0100

    Update Functions.sql

--- Functions.sql
+++ Functions.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION dbo.FUNC_ConferenceDiscount(@ConferenceID int, @OrderDate datetime) 
+CREATE FUNCTION dbo.ConferenceDiscount(@ConferenceID int, @OrderDate datetime) 
   RETURNS decimal(10,2) AS
 BEGIN
   RETURN
@@ -10,7 +10,7 @@ BEGIN
     )
 END
 
-CREATE FUNCTION dbo.FUNC_IsStudent(@PersonID int) 
+CREATE FUNCTION dbo.IsStudent(@PersonID int) 
   RETURNS bit AS
 BEGIN
   RETURN( CASE
@@ -21,13 +21,13 @@ BEGIN
       )
 END
 
-CREATE FUNCTION dbo.FUNC_TicketPrice(@TicketID int) 
+CREATE FUNCTION dbo.TicketPrice(@TicketID int) 
   RETURNS decimal(10,2) AS
 BEGIN
   RETURN
     (
-      SELECT c.BasePrice * dbo.FUNC_ConferenceDiscount(t.ConferenceID, o.OrderDate) / 100 *
-        dbo.FUNC_IsStudent(t.PersonID) * (1 - StudentDiscount) / 100
+      SELECT c.BasePrice * dbo.ConferenceDiscount(t.ConferenceID, o.OrderDate) / 100 *
+        dbo.IsStudent(t.PersonID) * (1 - StudentDiscount) / 100
       FROM Tickets AS t
       JOIN Orders AS o
            ON o.OrderID = t.OrderID
@@ -37,7 +37,7 @@ BEGIN
     )
 END
 
-CREATE FUNCTION dbo.FUNC_WorkshopsPrice(@TicketID) 
+CREATE FUNCTION dbo.WorkshopPrice(@TicketID) 
   RETURNS decimal(10,2) AS
 BEGIN
   RETURN
@@ -47,55 +47,5 @@ BEGIN
       JOIN Workshops AS w
            ON wr.WorkshopID = w.WorkshopID
       WHERE wr.TicketID = @TicketID
-    ) + FUNC_TicketPrice(@TicketID);
-END
-
-CREATE FUNCTION dbo.FUNC_ConferenceParticipants(@ConferenceID) 
-  RETURNS TABLE AS
-BEGIN
-  RETURN
-    (
-      SELECT Lastname + ' ' + Firstname AS Name, ISNULL(CompanyName, '')
-      FROM People AS p 
-      JOIN CompanyList AS cl
-           ON p.CompanyID = cl.CompanyID
-      JOIN Tickets AS t
-           ON t.PersonID = p.PersonID
-      WHERE t.ConferenceID = @ConferenceID
-    )
-END
-
-CREATE FUNCTION dbo.FUNC_WorkshopParticipants(@WorkshopID) 
-  RETURNS TABLE AS
-BEGIN
-  RETURN
-    (
-      SELECT Lastname + ' ' + Firstname AS Name, ISNULL(CompanyName, '')
-      FROM People AS p 
-      JOIN CompanyList AS cl
-           ON p.CompanyID = cl.CompanyID
-      JOIN Tickets AS t
-           ON t.PersonID = p.PersonID
-      JOIN WorkshopReservations AS wr
-           ON wr.TicketID = t.TicketID
-      WHERE wr.WorkshopID = @WorkshopID
-    )
-END
-
-
-CREATE FUNCTION dbo.FUNC_ConferenceDayParticipants(@ConferenceID, @Day datetime) 
-  RETURNS TABLE AS
-BEGIN
-  RETURN
-    (
-      SELECT Lastname + ' ' + Firstname AS Name, ISNULL(CompanyName, '')
-      FROM People AS p 
-      JOIN CompanyList AS cl
-           ON p.CompanyID = cl.CompanyID
-      JOIN Tickets AS t
-           ON t.PersonID = p.PersonID
-      JOIN ConferenceDays AS cd
-           ON cd.ConferenceID = t.ConferenceID
-      WHERE t.ConferenceID = @ConferenceID AND cd.Day = @Day 
     )
 END
