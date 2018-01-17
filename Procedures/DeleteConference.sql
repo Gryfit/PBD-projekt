1eb59255896d7bd2a9ddc789eeaa5a2b3d873fc4
Bartek <bartektonia@gmail.com>
2018-01-17 14:49:55 +0100

    Delete update

--- Procedures/DeleteConference.sql
+++ Procedures/DeleteConference.sql
@@ -13,24 +13,17 @@ AS
 	)
 	DELETE FROM dbo.Workshops WHERE ConferenceID = @ConferenceID
 
+	SELECT DISTINCT PersonID INTO TmpPeople FROM dbo.Tickets WHERE ConferenceID =@ConferenceID
 
-	DECLARE @TmpPeople TABLE (
-	PersonID INT
-	)
-	INSERT INTO @TmpPeople
-	SELECT DISTINCT PersonID FROM dbo.Tickets WHERE ConferenceID =@ConferenceID
-	DECLARE @TmpOrders TABLE (
-	OrderID INT
-	)
-	INSERT INTO @TmpOrders
-	SELECT DISTINCT OrderID FROM dbo.Tickets WHERE ConferenceID =@ConferenceID
+	SELECT DISTINCT OrderID INTO TmpOrders FROM dbo.Tickets WHERE ConferenceID =@ConferenceID
 
 	 
 	DELETE FROM dbo.Tickets WHERE ConferenceID = @ConferenceID
-	DELETE FROM dbo.People WHERE PersonID IN (SELECT * FROM @TmpPeople)
-	DELETE FROM dbo.Orders WHERE OrderID IN (SELECT * FROM @TmpOrders)
+	DELETE FROM dbo.People WHERE PersonID IN (SELECT * FROM TmpPeople)
+	DELETE FROM dbo.Orders WHERE OrderID IN (SELECT * FROM TmpOrders)
 	DELETE FROM dbo.ConferenceDays WHERE ConferenceID = @ConferenceID
 	DELETE FROM dbo.Conferences WHERE ConferenceID = @ConferenceID
-
+	DROP TABLE dbo.TmpPeople
+	DROP TABLE dbo.TmpOrders
 
 	 
