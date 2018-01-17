ee4061c1626790509ac5acc4218e410782ab0b77
Bartek <bartektonia@gmail.com>
2018-01-17 14:50:34 +0100

    Delete update

--- Procedures/DeleteOrder.sql
+++ Procedures/DeleteOrder.sql
@@ -1,9 +1,13 @@
 CREATE PROC DeleteOrder
 	@OrderID INT
 AS
-	DELETE FROM dbo.Payments WHERE OrderID = @OrderID
-	DELETE FROM dbo.WorkshopReservations WHERE TicketID IN (SELECT TicketID FROM dbo.Tickets WHERE OrderID = @OrderID)
 	DELETE FROM dbo.Students WHERE PersonID IN (SELECT PersonID FROM dbo.Tickets WHERE OrderID = @OrderID)
-	DELETE FROM dbo.People WHERE PersonID IN (SELECT PersonID FROM dbo.Tickets WHERE OrderID = @OrderID)
+	DELETE FROM dbo.WorkshopReservations WHERE TicketID IN (SELECT TicketID FROM dbo.Tickets WHERE OrderID = @OrderID)
+
+	(SELECT PersonID INTO TmpPeople FROM dbo.Tickets WHERE OrderID = @OrderID)
 
 	UPDATE dbo.Tickets SET OrderID = NULL, PersonID = NULL WHERE OrderID = @OrderID
+	DELETE FROM dbo.Payments WHERE OrderID = @OrderID
+
+	DELETE FROM dbo.People WHERE PersonID IN (SELECT * FROM TmpPeople)
+	DROP TABLE TmpPeople
