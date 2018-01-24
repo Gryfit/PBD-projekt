4b97c23e7dcc42467484a435b0fe8f898a166a77
karolBak <bak.karolek@gmail.com>
2018-01-25 00:49:57 +0100

    Enable adding a student.

--- Procedures/AddPerson.sql
+++ Procedures/AddPerson.sql
@@ -1,32 +1,41 @@
-CREATE PROC AddPerson
-	@CompanyName INT,
-	@LastName VARCHAR(30),
-	@FirstName VARCHAR(30),
-	@TicketID INT
+CREATE PROC dbo.AddPerson
+    @CompanyName INT,
+    @LastName VARCHAR(30),
+    @FirstName VARCHAR(30),
+    @IsStudent BIT
 AS
-	IF
-	@CompanyName IS NOT NULL
-		IF
-		(SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName) IS NULL
-		BEGIN
-			INSERT dbo.CompanyList
-			(
-			    CompanyName
-			)
-			VALUES
-			(@CompanyName -- CompanyName - varchar(50)
-			    )
-		END
-		INSERT dbo.People
-		(
-			CompanyID,
-			LastName,
-			FirstName
-		)
-		VALUES
-		(   (SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName),  -- CompanyID - int
-			@LastName, -- LastName - varchar(30)
-			@FirstName  -- FirstName - varchar(30)
-			)
-		UPDATE dbo.Tickets
-		SET dbo.Tickets.PersonID = (SELECT TOP 1 PersonID FROM dbo.People ORDER BY PersonID DESC)
+    IF @CompanyName IS NOT NULL
+        IF (SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName) IS NULL
+        BEGIN
+            INSERT dbo.CompanyList
+            (
+                CompanyName
+            )
+            VALUES
+            (
+                @CompanyName -- CompanyName - varchar(50)
+            )
+        END
+        
+    DECLARE @IdentityValue TABLE (
+        PersonID int
+        )
+    INSERT dbo.People
+    (
+        CompanyID,
+        LastName,
+        FirstName
+    )
+    OUTPUT inserted.PersonID INTO @IdentityValue
+    VALUES
+    (   (SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName),  -- CompanyID - int
+        @LastName, -- LastName - varchar(30)
+        @FirstName  -- FirstName - varchar(30)
+    )
+    
+    IF @IsStudent = 1
+    BEGIN
+        INSERT dbo.Students (PersonID)
+        SELECT PersonID 
+        FROM @IdentityValue   
+    END
