b89edcf7065f5db904e79463b74693e9b7c18056
karolBak <bak.karolek@gmail.com>
2018-01-24 19:58:50 +0100

    Make it possible to use defined PaymentDate.

--- Procedures/AddPayment.sql
+++ Procedures/AddPayment.sql
@@ -1,8 +1,13 @@
 CREATE PROC AddPayment
-	@OrderID INT,
-	@PaymentValue DECIMAL(10,2),
-	@BankAccount INT
+    @OrderID INT,
+    @PaymentDate DATETIME,
+    @PaymentValue DECIMAL(10,2),
+    @BankAccount INT
 AS
+IF @PaymentDate IS NULL
+BEGIN
+    SET @PaymentDate = GETDATE()
+END
 INSERT dbo.Payments
 (
     PaymentID,
@@ -14,7 +19,7 @@ INSERT dbo.Payments
 VALUES
 (   (SELECT COUNT(OrderID) FROM dbo.Payments WHERE OrderID=@OrderID) + 1,         -- PaymentID - int
     @OrderID,         -- OrderID - int
-    GETDATE(), -- PaymentDate - datetime
+    @PaymentDate, -- PaymentDate - datetime
     @PaymentValue,      -- PaymentValue - decimal(10, 2)
     @BankAccount          -- BankAccount - int
     )
