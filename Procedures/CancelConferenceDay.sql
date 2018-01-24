4dd3ff294b44b1c035e87ce45274fefbe8ca7482
karolBak <bak.karolek@gmail.com>
2018-01-24 21:46:08 +0100

    Added UPDATE for Workshops

--- Procedures/CancelConferenceDay.sql
+++ Procedures/CancelConferenceDay.sql
@@ -4,3 +4,4 @@ CREATE PROC CancelConferenceDay
 AS
 BEGIN
 	UPDATE dbo.ConferenceDays SET Cancelled = 1 WHERE ConferenceDayID=@ConferenceDayID AND ConferenceID = @ConferenceID
+	UPDATE dbo.Workshops SET Cancelled = 1 WHERE ConferenceDayID=@ConferenceDayID AND ConferenceID = @ConferenceID
