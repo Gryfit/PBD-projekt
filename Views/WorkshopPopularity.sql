c3c4d1421326237e8b210524dfcc97177f238a0e
karolBak <bak.karolek@gmail.com>
2018-01-25 00:37:50 +0100

    Update WorkshopPopularity.sql

--- Views/WorkshopPopularity.sql
+++ Views/WorkshopPopularity.sql
@@ -1,11 +1,11 @@
-CREATE VIEW VIEW_WorkshopPopularity
+CREATE VIEW dbo.WorkshopPopularity
 AS
   SELECT wr.WorkshopID, (COUNT(wr.WorkshopID) * 100 / w.Seats) AS 'PercentSold',
          COUNT(wr.WorkshopID) AS 'SeatsSold', w.Seats, w.Start, w.Duration
-  FROM WorkshopReservations AS wr
+  FROM dbo.WorkshopReservations AS wr
   JOIN Tickets AS t
        ON t.TicketID = wr.WorkshopID
-  JOIN Workshops AS w
+  JOIN dbo.Workshops AS w
        ON wr.WorkshopID = w.WorkshopID AND w.Cancelled = 0
   WHERE t.OrderID != NULL
   GROUP BY wr.WorkshopID, w.Seats
