db17871f1545fe36db23f95604ca2da239b1dbe3
karolBak <bak.karolek@gmail.com>
2018-01-25 12:38:44 +0100

    Update OrderCostDetails.sql

--- Views/OrderCostDetails.sql
+++ Views/OrderCostDetails.sql
@@ -1,10 +1,12 @@
-CREATE VIEW OrderCostDetails 
+ALTER VIEW OrderCostDetails 
 AS
-    SELECT o.OrderID, o.OrderDate, LastName, FirstName, dbo.IsStudent(t.PersonID) AS IsStudent,
+    SELECT o.OrderID, o.OrderDate, LastName + ' ' + FirstName AS 'name', CompanyName, dbo.IsStudent(t.PersonID) AS IsStudent,
     dbo.TicketPrice(t.TicketID) AS 'ConferencePrice', dbo.WorkshopPrice(t.TicketID) AS 'WorkshopPrice',
     dbo.TicketPrice(t.TicketID) + dbo.WorkshopPrice(t.TicketID) AS 'TotalPrice'
     FROM Tickets AS t
     JOIN Orders AS o
          ON o.OrderID = t.OrderID
     LEFT JOIN People AS p
-         ON p.PersonID = t.PersonID
+    ON p.PersonID = t.PersonID
+    LEFT JOIN CompanyList AS cl
+    ON cl.CompanyID = p.CompanyID
