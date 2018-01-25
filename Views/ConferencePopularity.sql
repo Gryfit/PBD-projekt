d2551a6c682dfcb3f2275d82271d87bd008403d7
karolBak <bak.karolek@gmail.com>
2018-01-25 12:18:38 +0100

    Update ConferencePopularity.sql

--- Views/ConferencePopularity.sql
+++ Views/ConferencePopularity.sql
@@ -1,9 +1,9 @@
 CREATE VIEW dbo.ConferencePopularity
 AS
-  SELECT c.ConferenceID, c.StartDate, c.EndDate, (COUNT(c.ConferenceID) / c.Seats * 100) AS 'PercentSold',
+  SELECT c.ConferenceID, c.StartDate, c.EndDate, (COUNT(c.ConferenceID) * 100 / c.Seats) AS 'PercentageSold',
          COUNT(c.ConferenceID) AS 'SeatsSold', c.Seats
   FROM Conferences AS c
   JOIN Tickets AS t
        ON t.ConferenceID = c.ConferenceID
-  WHERE t.OrderID != NULL AND c.Cancelled = 0
-  GROUP BY c.ConferenceID, Seats, c.StartDate, c.EndDate
+  WHERE (t.orderID IS NOT NULL) AND c.Cancelled = 0
+GROUP BY c.ConferenceID, Seats, c.StartDate, c.EndDate
