3988153d8e26d566a6d96e9370d99fffe0851b94
karolBak <bak.karolek@gmail.com>
2018-01-25 05:41:37 +0100

    Update OrderCost.sql

--- Functions/OrderCost.sql
+++ Functions/OrderCost.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION OrderCost(@OrderID int)
 RETURNS decimal(10,2) AS
 BEGIN
 RETURN (
-    SELECT SUM(dbo.TicketPrice (t.TicketID) + dbo.WorkshopPrice(t.TicketID))
+    SELECT TOP 1 SUM(dbo.TicketPrice (t.TicketID) + dbo.WorkshopPrice(t.TicketID))
     FROM Tickets AS t
     WHERE t.OrderID = @OrderID
     GROUP BY OrderID, TicketID
