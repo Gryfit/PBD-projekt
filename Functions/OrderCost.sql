353736856fed536e35be3fbddd2d4b5b92400ca8
karolBak <bak.karolek@gmail.com>
2018-01-25 03:55:01 +0100

    Create OrderCost.sql

---
+++ Functions/OrderCost.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION OrderCost(@OrderID int) 
+AS
+    SELECT o.OrderID, o.OrderDate, LastName, FirstName, dbo.IsStudent(t.PersonID) AS IsStudent,
+    dbo.TicketPrice(t.TicketID) AS 'ConferencePrice', dbo.WorkshopPrice(t.TicketID) AS 'WorkshopPrice',
+    dbo.TicketPrice(t.TicketID) + dbo.WorkshopPrice(t.TicketID) AS 'TotalPrice'
+    FROM Tickets AS t
+    JOIN Orders AS o
+         ON o.OrderID = t.OrderID
+    LEFT JOIN People AS p
+         ON p.PersonID = t.PersonID
