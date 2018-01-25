52d1066d6aca0d427f8f5aa81d21627f2d38f776
karolBak <bak.karolek@gmail.com>
2018-01-25 13:34:34 +0100

    Update ClientPayments.sql

--- Views/ClientPayments.sql
+++ Views/ClientPayments.sql
@@ -1,12 +1,13 @@
-ALTER VIEW ClientPayments AS
-    SELECT oc.OrderID, o.OrderDate, (SELECT Top 1 PaymentDate FROM Payments AS p WHERE p.OrderID = oc.OrderID ORDER BY PaymentDate DESC) AS 'LastPayment',
-        ConferenceID, CompanyName, Phone, SUM(TotalPrice) AS HasToPay, 
-        (SELECT SUM(PaymentValue) FROM Payments AS p WHERE p.OrderID = oc.OrderID GROUP BY p.OrderID) AS AlreadyPaid
-    FROM OrderCostDetails AS oc
-    JOIN Orders AS o
-        ON o.OrderID = oc.OrderID
+CREATE VIEW ClientPayments AS
+    SELECT o.OrderID, o.OrderDate, ConferenceID, CompanyName, Phone, dbo.ordercost(o.OrderID) AS 'HasToPay'
+      --  (SELECT SUM(PaymentValue) FROM Payments AS p WHERE p.OrderID = oc.OrderID GROUP BY p.OrderID) AS AlreadyPaid
+    FROM Orders AS o
     JOIN Clients AS c
         ON c.ClientID = o.ClientID
     JOIN Tickets AS t
         ON t.OrderID = o.OrderID
-    GROUP BY oc.OrderID, CompanyName, TotalPrice, o.OrderDate, Phone, ConferenceID
+    LEFT JOIN CompanyClients AS cc
+        ON cc.ClientID = c.CLientID
+    LEFT JOIN CompanyList AS cl
+        ON cl.CompanyID = cc.CompanyID
+    GROUP BY o.OrderID, CompanyName, o.OrderDate, Phone, ConferenceID
