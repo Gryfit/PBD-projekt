7b9ad703112fc9c38f938858d62d092f2a3b7d0d
karolBak <bak.karolek@gmail.com>
2018-01-25 14:18:58 +0100

    Update TicketPrice.sql

--- Functions/TicketPrice.sql
+++ Functions/TicketPrice.sql
@@ -2,13 +2,16 @@ CREATE FUNCTION dbo.TicketPrice(@TicketID int) RETURNS decimal(10,2) AS
 BEGIN
   RETURN
     (
-      SELECT c.BasePrice * (1 - dbo.ConferenceDiscount(t.ConferenceID, o.OrderDate) / 100) 
-      * (1 - dbo.IsStudent(t.PersonID) * c.StudentDiscount / 100)
+      SELECT sum(c.BasePrice * (1 - dbo.ConferenceDiscount(t.ConferenceID, o.OrderDate) / 100) 
+      * (1 - dbo.IsStudent(t.PersonID) * c.StudentDiscount / 100) * cd.Cancelled)
       FROM Tickets AS t
       JOIN Orders AS o
            ON o.OrderID = t.OrderID
       JOIN Conferences AS c
            ON c.ConferenceID = t.ConferenceID
+      JOIN ConferenceDays AS cd
+           ON cd.ConferenceID = t.ConferenceID 
       WHERE t.TicketID = @TicketID
+      GROUP BY t.ConferenceID
     )
 END
