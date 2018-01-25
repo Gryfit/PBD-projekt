909ee90963cdbf9c7c2ad4d0284723a509fbaccd
karolBak <bak.karolek@gmail.com>
2018-01-25 13:37:04 +0100

    Update ClientPayments.sql

--- Views/ClientPayments.sql
+++ Views/ClientPayments.sql
@@ -1,6 +1,9 @@
-CREATE VIEW ClientPayments AS
-    SELECT o.OrderID, o.OrderDate, ConferenceID, CompanyName, Phone, dbo.ordercost(o.OrderID) AS 'HasToPay'
-      --  (SELECT SUM(PaymentValue) FROM Payments AS p WHERE p.OrderID = oc.OrderID GROUP BY p.OrderID) AS AlreadyPaid
+ALTER VIEW ClientPayments AS
+    SELECT o.OrderID, o.OrderDate, 
+    (SELECT Top 1 PaymentDate FROM Payments AS p WHERE p.OrderID = o.OrderID ORDER BY PaymentDate DESC) AS 'LastPayment',
+    ConferenceID, CompanyName, Phone, 
+    dbo.ordercost(o.OrderID) AS 'HasToPay',
+      (SELECT SUM(PaymentValue) FROM Payments AS p WHERE p.OrderID = o.OrderID GROUP BY p.OrderID) AS AlreadyPaid
     FROM Orders AS o
     JOIN Clients AS c
         ON c.ClientID = o.ClientID
