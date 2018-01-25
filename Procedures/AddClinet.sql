9784972f441cba3b8fe31d1d743222d0269893cd
karolBak <bak.karolek@gmail.com>
2018-01-25 06:23:49 +0100

    Update AddClinet.sql

--- Procedures/AddClinet.sql
+++ Procedures/AddClinet.sql
@@ -1,45 +1,41 @@
-CREATE PROC AddClient
-	@Email VARCHAR(255),
-	@Phone VARCHAR(14),
-	@CompanyName  VARCHAR(50)
+ALTER PROC AddClient
+    @Email VARCHAR(255),
+    @Phone VARCHAR(14),
+    @CompanyName  VARCHAR(50)
 AS
-	INSERT dbo.Clients
-	(
-	    Email,
-	    Phone
-	)
-	VALUES
-	(   @Email, -- Email - varchar(255)
-	    @Phone  -- Phone - varchar(14)
-	    )
-	IF @CompanyName IS NOT NULL
-		IF (SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName) IS NOT NULL
-			INSERT dbo.CompanyClients
-			(
-			    ClientID,
-			    CompanyID
-			)
-			VALUES
-			(   (SELECT ClientID 
-			     FROM dbo.CompanyClients as cc 
-			     JOIN CompanyList as cl 
-			          ON cl.CompanyID = cc.CompanyID AND CompanyName = @CompanyName), -- ClientID - int
-			    (SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName)  -- CompanyID - int
-			    )
-		ELSE
-			INSERT dbo.CompanyList
-			(
-			    CompanyName
-			)
-			VALUES
-			(@CompanyName -- CompanyName - varchar(50)
-			    )
-			INSERT dbo.CompanyClients
-			(
-			    ClientID,
-			    CompanyID
-			)
-			VALUES
-			(   (SELECT TOP 1 ClientID FROM dbo.Clients ORDER BY ClientID DESC), -- ClientID - int
-			    (SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName)  -- CompanyID - int
-			    )
+    DECLARE @compID AS int
+    SET @compID = (SELECT CompanyID FROM dbo.CompanyList WHERE CompanyName = @CompanyName)
+    DECLARE @cliID AS int
+    SET @cliID = (SELECT ClientID FROM dbo.CompanyClients WHERE CompanyID = @compID)
+    
+
+    IF @CompanyName IS NULL
+    INSERT dbo.Clients
+    (
+        Email,
+        Phone
+    )
+    VALUES
+    (   @Email, -- Email - varchar(255)
+        @Phone  -- Phone - varchar(14)
+        )
+    ELSE
+        IF @cliID IS NULL
+            INSERT dbo.Clients
+            (
+                 Email,
+                 Phone
+            )
+            VALUES
+            (   @Email, -- Email - varchar(255)
+                @Phone  -- Phone - varchar(14)
+            )
+            INSERT dbo.CompanyClients
+            (
+                ClientID,
+                CompanyID
+            )
+            VALUES
+            (   (SELECT Top 1 ClientID FROM Clients ORDER BY 1 DESC), -- ClientID - int
+                @compID  -- CompanyID - int
+                )
