5726deefe05953af9a40bd62603bb931f33e430b
karolBak <bak.karolek@gmail.com>
2018-01-25 13:45:45 +0100

    Update OrderTickets.sql

--- Views/OrderTickets.sql
+++ Views/OrderTickets.sql
@@ -1,14 +1,19 @@
 CREATE VIEW dbo.OrderTickets
 AS
-  SELECT o.OrderID, o.OrderDate,
+  SELECT c.ClientID, CompanyName,
          c.Email, c.Phone,
          (SELECT COUNT (*) FROM Tickets WHERE OrderID = o.OrderID) AS 'TicketsOrdered',
          (SELECT COUNT (*) 
           FROM Tickets AS t 
           JOIN Students AS s 
                ON s.PersonID = t.PersonID
+          WHERE t.OrderID = o.OrderID
          ) AS 'StudentTickets'
   FROM Orders AS o
   JOIN Clients AS c
        ON c.ClientID = o.ClientID
+  LEFT JOIN CompanyClients AS cc
+      ON cc.ClientID = c.ClientID
+  LEFT JOIN CompanyList AS cl
+      ON cc.CompanyID = cl.CompanyID
        
